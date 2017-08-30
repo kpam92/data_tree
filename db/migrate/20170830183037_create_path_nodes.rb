1c0cf59404c0cf9c4f2ee0bb908f42fc00027b75
kpam92 <kpam1992@gmail.com>
2017-08-30 11:32:33 -0700

    pre migration pathnode

--- db/migrate/20170830183037_create_path_nodes.rb
+++ db/migrate/20170830183037_create_path_nodes.rb
@@ -1,7 +1,8 @@
 class CreatePathNodes < ActiveRecord::Migration[5.0]
   def change
     create_table :path_nodes do |t|
-
+      t.string :path, null: false
+      t.integer :parent_id
       t.timestamps
     end
   end
