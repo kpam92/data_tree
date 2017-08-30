8ff78ac3e23f76fb3eb11a9bb3be36a00fdca881
kpam92 <kpam1992@gmail.com>
2017-08-30 11:26:13 -0700

    pre-migration path

--- db/migrate/20170830182511_create_paths.rb
+++ db/migrate/20170830182511_create_paths.rb
@@ -1,7 +1,8 @@
 class CreatePaths < ActiveRecord::Migration[5.0]
   def change
     create_table :paths do |t|
-
+      t.string :curr_path, null: false
+      t.integer :children, null: false
       t.timestamps
     end
   end
