ad7c0b9a28f2190f613a5b28fc72e92ea1805113
kpam92 <kpam1992@gmail.com>
2017-08-30 15:58:51 -0700

    nodes controller

--- app/controllers/api/nodes_controller.rb
+++ app/controllers/api/nodes_controller.rb
@@ -1,2 +1,10 @@
 class Api::NodesController < ApplicationController
+
+  def index
+    @nodes = Node.all
+  end
+
+  def show
+    @node = Node.find(params[:id])
+  end
 end
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,10 +2,31 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import TreeContainer from './tree/tree_container';
 
-const Root = ({ store }) => (
-  <Provider store={ store }>
-    <TreeContainer/>
-  </Provider>
-);
+// const Root = ({ store }) => (
+//   <Provider store={ store }>
+//     <TreeContainer/>
+//   </Provider>
+// );
+
+class Root extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    debugger;
+  }
+
+  render() {
+
+    var {store} = this.props;
+
+    return(
+      <Provider store={ store }>
+        <TreeContainer/>
+      </Provider>
+    )
+  }
+}
 
 export default Root;
