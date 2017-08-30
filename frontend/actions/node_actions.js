cb2acf627c518f6fc0ea4e2d20aa4972612552c2
kpam92 <kpam1992@gmail.com>
2017-08-30 16:41:09 -0700

    node actions

--- frontend/actions/node_actions.js
+++ frontend/actions/node_actions.js
@@ -1,37 +1,29 @@
 import * as APIUtil from '../util/photo_api_util'
 
-export const RECEIVE_PHOTOS = 'RECEIVE_PHOTOS';
+export const RECEIVE_CHILD_NODES = 'RECEIVE_CHILD_NODES';
+export const UPDATE_NODE = 'UPDATE_NODE';
 
-export const RECEIVE_PHOTO = 'RECEIVE_PHOTO';
-export const UPDATE_PHOTO = 'UPDATE_PHOTO';
-
-export const receivePhotos = photos => ({
-  type: RECEIVE_PHOTOS,
+export const receiveChildNodes = nodes => ({
+  type: RECEIVE_CHILD_NODES,
   photos
 });
 export const receivePhoto = photo => ({
   type: RECEIVE_PHOTO,
   photo
 });
-export const updatePhoto = photo => ({
-  type: UPDATE_PHOTO,
-  photo
+export const updateNode = node => ({
+  type: UPDATE_NODE,
+  node
 });
 
-export const fetchPhotos = () => dispatch => (
-  APIUtil.fetchPhotos().then(photos => (
-    dispatch(receivePhotos(photos))
-  ))
-);
-
-export const addPhoto = (photo) => dispatch => (
-  APIUtil.addPhoto(photo).then(photo => (
-    dispatch(receivePhoto(photo))
+export const fetchChildren = (id) => dispatch => (
+  APIUtil.fetchChildren(id).then(nodes => (
+    dispatch(receiveChildNodes(nodes))
   ))
 );
 
 export const fetchInitialNode = () => dispatch => (
-  APIUtil.fetchPhoto(id).then(photo => (
-    dispatch(updatePhoto(photo))
+  APIUtil.fetchNode(67).then(node => (
+    dispatch(updateNode(node))
   ))
 );
